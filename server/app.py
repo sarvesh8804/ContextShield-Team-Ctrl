8be8255a168f6c2ffd74a82de743e7deb0b9e02f
sarvesh8804 <sarvesh.huddar1@gmail.com>
2026-04-02 00:59:10 +0530

    import bug fix

--- server/app.py
+++ server/app.py
@@ -12,7 +12,7 @@ if str(ROOT) not in sys.path:
     sys.path.insert(0, str(ROOT))
 
 from openenv.core.env_server.http_server import create_fastapi_app
-from fastapi import HTMLResponse
+from fastapi.responses import HTMLResponse
 
 from env.openenv_adapter import ContextShieldOpenEnv
 from models.action import Action
